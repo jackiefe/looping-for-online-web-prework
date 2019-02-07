4ddd9319875780f661d578096eba5559bd3cbe87
Jacqi E <jackie.entz@gmail.com>
2019-02-07 06:46:19 +0000

    Done.

--- for.rb
+++ for.rb
@@ -1,7 +1,11 @@
-
+require 'pry'
 def using_for
-	checklist = 1..10
-	#your code here
+	checklist = 1..10 
+	for item in checklist
+	  puts "Wingardium Leviosa"
+	end
+  #binding.pry 
+	#range #your code here
 end
 
 
